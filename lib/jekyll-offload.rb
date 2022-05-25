4a976c2297d85e5c47803201452da21decb03a23
Joe Buhlig <joe@joebuhlig.com>
2022-05-25 13:27:31 -0500

    upload thumbnails and squares as well

--- lib/jekyll-offload.rb
+++ lib/jekyll-offload.rb
@@ -5,34 +5,49 @@ require 'fileutils'
 
 module JekyllOffload
   def self.offload()
+    Dir['media/**/*.*'].each do |file|
+      push_to_s3(file)
+      File.delete("thumbnails/#{file}")
+      File.delete("square/#{file}")
+      File.delete(file)
+    end
+      FileUtils.remove_dir("thumbnails")
+      FileUtils.remove_dir("square")
+  end
+
+  def self.push_to_s3(file)
     s3 = Aws::S3::Client.new(
       access_key_id: ENV["S3_ACCESS_KEY"],
       secret_access_key: ENV["S3_SECRET_KEY"],
       endpoint: ENV["S3_OFFLOAD_ENDPOINT"],
       region: ENV["S3_OFFLOAD_REGION"]
     )
-
-    Dir['media/**/*.*'].each do |file|
-      file_type = MIME::Types.type_for(file.split('.').last).first.to_s
-      if file_type == "image/jpeg"
-        path = File.dirname(file)
-        FileUtils.mkdir_p("thumbnails/#{path}") unless File.directory?("thumbnails/#{path}")
-        puts "Creating thumbnail: thumbnails/#{file}"
-        convert = MiniMagick::Tool::Magick.new
-        convert << file
-        convert.resize("x700")
-        convert << "thumbnails/#{file}"
-        convert.call
-        obj = Aws::S3::Object.new(client: s3, bucket_name: ENV["S3_OFFLOAD_BUCKET"], key: "thumbnails/#{file}")
-        upload = obj.put({acl: "public-read", body: File.read("thumbnails/#{file}"), content_type: file_type})
-        File.delete("thumbnails/#{file}")
-      end
-      FileUtils.remove_dir("thumbnails")
-      puts "Uploading: #{file}"
-      obj = Aws::S3::Object.new(client: s3, bucket_name: ENV["S3_OFFLOAD_BUCKET"], key: file)
-      upload = obj.put({acl: "public-read", body: File.read(file), content_type: file_type})
-      File.delete(file)
+    file_type = MIME::Types.type_for(file.split('.').last).first.to_s
+    if file_type == "image/jpeg"
+      path = File.dirname(file)
+      FileUtils.mkdir_p("thumbnails/#{path}") unless File.directory?("thumbnails/#{path}")
+      FileUtils.mkdir_p("square/#{path}") unless File.directory?("square/#{path}")
+      puts "Creating thumbnail: thumbnails/#{file}"
+      convert = MiniMagick::Tool::Magick.new
+      convert << file
+      convert.resize("x700")
+      convert << "thumbnails/#{file}"
+      convert.call
+      obj = Aws::S3::Object.new(client: s3, bucket_name: ENV["S3_OFFLOAD_BUCKET"], key: "thumbnails/#{file}")
+      upload = obj.put({acl: "public-read", body: File.read("thumbnails/#{file}"), content_type: file_type})
+puts "Creating square: square/#{file}"
+      convert = MiniMagick::Tool::Magick.new
+      convert << file
+      convert.merge! ["-thumbnail", "1080x1080^", "-gravity", "center", "-extent", "1080x1080", "-auto-orient"]
+      convert << "square/#{file}"
+      convert.call
+      obj = Aws::S3::Object.new(client: s3, bucket_name: ENV["S3_OFFLOAD_BUCKET"], key: "square/#{file}")
+      upload = obj.put({acl: "public-read", body: File.read("square/#{file}"), content_type: file_type})
     end
+    puts "Uploading: #{file}"
+    obj = Aws::S3::Object.new(client: s3, bucket_name: ENV["S3_OFFLOAD_BUCKET"], key: file)
+    upload = obj.put({acl: "public-read", body: File.read(file), content_type: file_type})
+    file
   end
 end
 
